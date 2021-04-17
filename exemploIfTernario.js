f4aaec9ad4576227079b9888aeda779cf8299409
Denis Batista <denis.si2011@gmail.com>
2021-04-17 15:38:40 -0300

    Aprendendo estrutura condicionais em javascript.

---
+++ exemploIfTernario.js
@@ -0,0 +1,13 @@
+// Converncional
+
+// var status = "";
+// if(nota > 7){
+//     status = "Aprovado";
+// }else {
+//     status = "Reprovado";
+// }
+
+//Ternário
+var status = nota > 7 ? "Aprovado" : "Reprovado";
+
+var mensagem = (mes === 11 || mes == 12) ? "Promoção" : "Preço normal";
